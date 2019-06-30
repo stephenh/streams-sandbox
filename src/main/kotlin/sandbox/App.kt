11ea77756b90ff46bb214035176362ad44b3803b
Stephen Haberman <stephen.haberman@gmail.com>
2019-06-30 12:55:29 -0500

    Orgs/OrgUsers.

--- src/main/kotlin/sandbox/App.kt
+++ src/main/kotlin/sandbox/App.kt
@@ -19,6 +19,17 @@ data class User(
   val lastName: String
 )
 
+data class Org(
+  val id: Long,
+  val name: String
+)
+
+data class OrgUser(
+  val id: Long,
+  val userId: Long,
+  val orgId: Long
+)
+
 class App {
   val greeting: String
     get() {
@@ -33,29 +44,42 @@ class App {
     props[StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG] = Serdes.String().javaClass.name
     props[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
 
+    // Create the serializers/deserializers
     val mapper = ObjectMapper()
       .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
       .enable(SerializationFeature.INDENT_OUTPUT)
       .registerModule(KotlinModule())
     val userSerdes = Jackson2Serde(mapper, User::class.java)
+    val orgSerdes = Jackson2Serde(mapper, Org::class.java)
+    val orgUserSerdes = Jackson2Serde(mapper, OrgUser::class.java)
     val strSerializer = Serdes.String().serializer()
-    val strDeserializer = Serdes.String().deserializer()
 
+    // Create streams for each of the CDC topics
     val builder = StreamsBuilder()
-    val usersStream = builder.stream<String, User>("users", Consumed.with(Serdes.String(), userSerdes))
+    val usersStream = builder.stream("users", Consumed.with(Serdes.Long(), userSerdes))
+    val orgStream = builder.stream("orgs", Consumed.with(Serdes.Long(), orgSerdes))
+    val orgUserStream = builder.stream("org_users", Consumed.with(Serdes.Long(), orgUserSerdes))
+
     val filtered = usersStream.filter { _, value -> value.lastName.startsWith("k") }
     filtered.to("output-topic")
 
     val topology = builder.build()
     val driver = TopologyTestDriver(topology, props)
 
-    val factory = ConsumerRecordFactory(strSerializer, strSerializer)
-    driver.pipeInput(factory.create("users", "key", mapper.writeValueAsString(User(1, "kid"))))
+    val factory = ConsumerRecordFactory(Serdes.Long().serializer(), strSerializer)
+    driver.pipeInput(factory.create("users", 1L, mapper.writeValueAsString(User(1, "kid"))))
 
-    val record = driver.readOutput("output-topic", strDeserializer, userSerdes.deserializer())
+    val record = driver.readOutput("output-topic", Serdes.Long().deserializer(), userSerdes.deserializer())
     println(record)
 
-    val users = builder.table("users", Consumed.with(Serdes.String(), userSerdes))
+    val users = builder.table("users", Consumed.with(Serdes.Long(), userSerdes))
+
+    val orgTable = builder.table("orgs", Consumed.with(Serdes.Long(), orgSerdes))
+
+    val orgUserTable = builder.table("org_users", Consumed.with(Serdes.Long(), orgUserSerdes))
+
+    
+
   }
 
 }
